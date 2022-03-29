0213118e35f51daf38d01b5f65a094cd9ebb4f94
dumutarares <dumutarares@gmail.com>
2022-03-29 13:14:15 +0300

    Adding tests

--- src/test/java/repository/AbstractXMLRepositoryTest.java
+++ src/test/java/repository/AbstractXMLRepositoryTest.java
@@ -2,19 +2,24 @@ package repository;
 
 import domain.Student;
 import org.junit.jupiter.api.Test;
-import validation.StudentValidator;
-import validation.Validator;
+import org.junit.platform.commons.util.StringUtils;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-class AbstractXMLRepositoryTest {
+public class AbstractXMLRepositoryTest {
 
     @Test
     public void Test_save_student_when_id_is_null_returns_null() {
         Student student = new Student(null, "Rares", 200,"rares@gmail.com");
         StudentXMLRepo studentXMLRepository = new StudentXMLRepo( "src/main/resources/fisiere/Studenti.xml");
-
         assertNull(studentXMLRepository.save(student));
+    }
+
+    @Test
+    public void Test_save_student_when_id_is_empty() {
+        Student student = new Student("", "Rares", 200,"rares@gmail.com");
+        StudentXMLRepo studentXMLRepository = new StudentXMLRepo( "src/main/resources/fisiere/Studenti.xml");
+        assertEquals(student,studentXMLRepository.save(student));
     }
 
     @Test
@@ -23,7 +28,32 @@ class AbstractXMLRepositoryTest {
         StudentXMLRepo studentXMLRepository = new StudentXMLRepo( "src/main/resources/fisiere/Studenti.xml");
 
         assertNull(studentXMLRepository.save(student));
-
         studentXMLRepository.delete("55");
     }
+
+    @Test
+    public void Test_save_student_when_email_is_null() {
+        Student student = new Student("1", "Rares", 200,null);
+        StudentXMLRepo studentXMLRepository = new StudentXMLRepo( "src/main/resources/fisiere/Studenti.xml");
+
+        assertEquals(student,studentXMLRepository.save(student));
+    }
+
+    @Test
+    public void Test_save_student_when_email_is_empty() {
+        Student student = new Student("1", "Rares", 200,"");
+        StudentXMLRepo studentXMLRepository = new StudentXMLRepo( "src/main/resources/fisiere/Studenti.xml");
+        assertEquals(student,studentXMLRepository.save(student));
+    }
+
+    @Test
+    public void Test_save_student_when_grupa_negative() {
+        Student student = new Student("1", "Rares", -1,"rares@gmail.com");
+        StudentXMLRepo studentXMLRepository = new StudentXMLRepo( "src/main/resources/fisiere/Studenti.xml");
+
+        assertEquals(student,studentXMLRepository.save(student));
+    }
+
+
+
 }
